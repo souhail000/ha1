a339d64bf0ed18b57bf37a944d1371972b84ed77
souhail000 <Souhail.belkheir@Student.HTW-Berlin.de>
2025-10-15 14:32:51 +0200

    neuer test

--- src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import htw.berlin.prog2.ha1.Calculator;
 
 @DisplayName("Retro calculator")
 class CalculatorTest {
@@ -90,5 +91,18 @@ class CalculatorTest {
 
 
     //TODO hier weitere Tests erstellen
+
+    @Test
+    @DisplayName("should display result after subtracting two positive multi-digit numbers")
+    void testPositiveSubtraction() {
+        Calculator calc = new Calculator();
+        calc.pressDigitKey(5);
+        calc.pressDigitKey(2);
+        calc.pressBinaryOperationKey("-");
+        calc.pressDigitKey(2);
+        calc.pressDigitKey(0);
+        calc.pressEqualsKey();
+        String expected = "32";
+        String actual = calc.readScreen();
 }
 
