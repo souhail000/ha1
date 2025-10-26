d2de3d147c899e97faad3eeeb3aab8fd0e68710e
souhail000 <Souhail.belkheir@Student.HTW-Berlin.de>
2025-10-26 15:55:09 +0100

    bugfix nach equal gleichbleiben

--- src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -123,6 +123,8 @@ public class Calculator {
             case "-" -> latestValue - Double.parseDouble(screen);
             case "x" -> latestValue * Double.parseDouble(screen);
             case "/" -> latestValue / Double.parseDouble(screen);
+            //bugix test 2 gleichzeichen
+            case "" -> Double.parseDouble(screen);
             default -> throw new IllegalArgumentException();
         };
         screen = Double.toString(result);
